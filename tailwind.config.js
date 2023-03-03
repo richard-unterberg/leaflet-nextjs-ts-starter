cb29f367555ce979571c1a949f65077140a7be1c
Richard Unterberg <mail@richardunterberg.de>
2023-03-03 18:34:00 +0100

    🔧 add app color to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: colors.sky[700],
+      secondary: colors.slate[600],
+      dark: colors.slate[900],
+      light: colors.slate[200],
+      white: colors.slate[50],
+    },
   },
   plugins: [],
 }
