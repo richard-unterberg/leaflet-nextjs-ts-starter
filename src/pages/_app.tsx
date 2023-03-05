0d685a8f24e4e4a062b482bfe03c6bdbd41e7841
Richard Unterberg <mail@richardunterberg.de>
2023-03-05 17:38:11 +0100

    🍱 Add custom for body base font (Catamaran)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import 'leaflet/dist/leaflet.css'
 import type { AppProps } from 'next/app'
+import { Catamaran } from 'next/font/google'
 
 import '@src/globals.css'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const catamaran = Catamaran({
+  subsets: ['latin'],
+  variable: '--font-catamaran',
+})
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <main className={`${catamaran.variable} font-sans text-base`}>
+    <Component {...pageProps} />
+  </main>
+)
 
 export default App
